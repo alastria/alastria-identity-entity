c3426bea40667e070ba314de67efe2c9e1b476bc
samuelsan95 <sanchezlopezsamuel@gmail.com>
2019-11-28 11:00:40 +0100

    rename project SP-front --> spFrontend

--- spFrontend/src/app/app.component.spec.ts
+++ spFrontend/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'SP-front'`, () => {
+  it(`should have as title 'spFrontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('SP-front');
+    expect(app.title).toEqual('spFrontend');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to SP-front!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to spFrontend!');
   });
 });
