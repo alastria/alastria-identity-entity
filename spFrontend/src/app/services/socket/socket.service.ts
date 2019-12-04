40d71f5ac9e89355b4f47958fc7d2f6f9e320305
samuelsan95 <sanchezlopezsamuel@gmail.com>
2019-12-04 12:31:51 +0100

    create socket service

---
+++ spFrontend/src/app/services/socket/socket.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Observer } from 'rxjs';
+
+import { Event } from 'src/app/models/enums/enums.model';
+
+import * as socketIo from 'socket.io-client';
+
+
+const webSocket = WebSocket; // Here we stub out the window object
+
+const SERVER_URL = 'ws://localhost:8080';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+  constructor() {
+
+  }
+  private socket: any;
+
+  public initSocket(): void {
+    this.socket = socketIo(SERVER_URL);
+  }
+
+  public send(message: any): void {
+    console.log('message service ', message);
+    this.socket.emit('message', message);
+  }
+
+  public onMessage(): Observable<any> {
+      return new Observable<any>(observer => {
+          this.socket.on('message', (data: any) => observer.next(data));
+      });
+  }
+
+  public onEvent(event: Event): Observable<any> {
+      return new Observable<Event>(observer => {
+          this.socket.on(event, () => observer.next());
+      });
+  }
+}
