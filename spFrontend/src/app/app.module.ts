faf9fa7524ec9bc456d4d49e8bfc2ee6520996fe
samuelsan95 <sanchezlopezsamuel@gmail.com>
2019-12-05 16:14:22 +0100

    add profile component in app module

--- spFrontend/src/app/app.module.ts
+++ spFrontend/src/app/app.module.ts
@@ -10,15 +10,17 @@ import { HomeModule } from './modules/home/home.module';
 
 // COMPONENTS
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { ProfileComponent } from './components/pages/profile/profile.component';
 
 // SERVICES
 import { AuthGuardService } from './services/auth/auth-guard.service';
-import { HeaderComponent } from './components/shared/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
