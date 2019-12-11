d688f88a299b1cadda8098d6e438daf0983312da
Víctor Nieves Sánchez <vnievess@gmail.com>
2019-12-11 10:26:07 +0100

    Revert "added getPublicKey function"
    This reverts commit f437ebf3497cafb2a3030b31d636ef1060f75090.

--- spBackend/api/models/serviceProvider.model.js
+++ spBackend/api/models/serviceProvider.model.js
@@ -5,12 +5,6 @@ const {transactionFactory, UserIdentity, config, tokensFactory} = require('alast
 const web3Helper = require('../helpers/web3.helper')
 
 const web3 = web3Helper.getWeb3()
-const keythereum = require('keythereum')
-const Web3 = require('web3')
-
-let ganacheNode = "http://127.0.0.1:8545";
-const web3 = new Web3(new Web3.providers.HttpProvider(ganacheNode))
-let subjectProxyAddress = "0xd7aa62f167c53f6c4ad9525f8be147a6eec9a58e";
 
 module.exports = {
   createAlastriaID,
@@ -34,20 +28,6 @@ function addSubjectCredential() {
 
 function getCurrentPublicKey() {
   return new Promise((resolve, reject) => {
-    if(configData.subject == undefined) {
-      console.log('You must create an Alastria ID')
-      process.exit()
-    }
-  
-  let currentPubKey = transactionFactory.publicKeyRegistry.getCurrentPublicKey(web3, subjectProxyAddress)
-  
-  web3.eth.call(currentPubKey)
-  .then(result => {
-    let publicKey = web3.utils.hexToUtf8(result)
-    console.log('RESULT ----->', publicKey.substr(1))
-  })
-  .catch(error => {
-    console.log('Error -------->', error)
-  })
+    // put here your code
   })
 }
