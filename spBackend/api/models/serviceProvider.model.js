f437ebf3497cafb2a3030b31d636ef1060f75090
rober12 <robertgar1993@hotmail.com>
2019-12-11 10:10:43 +0100

    added getPublicKey function

--- spBackend/api/models/serviceProvider.model.js
+++ spBackend/api/models/serviceProvider.model.js
@@ -5,6 +5,12 @@ const {transactionFactory, UserIdentity, config, tokensFactory} = require('alast
 const web3Helper = require('../helpers/web3.helper')
 
 const web3 = web3Helper.getWeb3()
+const keythereum = require('keythereum')
+const Web3 = require('web3')
+
+let ganacheNode = "http://127.0.0.1:8545";
+const web3 = new Web3(new Web3.providers.HttpProvider(ganacheNode))
+let subjectProxyAddress = "0xd7aa62f167c53f6c4ad9525f8be147a6eec9a58e";
 
 module.exports = {
   createAlastriaID,
@@ -28,6 +34,20 @@ function addSubjectCredential() {
 
 function getCurrentPublicKey() {
   return new Promise((resolve, reject) => {
-    // put here your code
+    if(configData.subject == undefined) {
+      console.log('You must create an Alastria ID')
+      process.exit()
+    }
+  
+  let currentPubKey = transactionFactory.publicKeyRegistry.getCurrentPublicKey(web3, subjectProxyAddress)
+  
+  web3.eth.call(currentPubKey)
+  .then(result => {
+    let publicKey = web3.utils.hexToUtf8(result)
+    console.log('RESULT ----->', publicKey.substr(1))
+  })
+  .catch(error => {
+    console.log('Error -------->', error)
+  })
   })
 }
