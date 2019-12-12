a192f47da29299501f00cc7f4e20d008bfc44397
rober12 <robertgar1993@hotmail.com>
2019-12-12 13:21:08 +0100

    added function addSubjectCredential not working

--- spBackend/api/models/serviceProvider.model.js
+++ spBackend/api/models/serviceProvider.model.js
@@ -4,6 +4,23 @@ const Log = require('log')
 const { transactionFactory, UserIdentity, config, tokensFactory } = require('alastria-identity-lib')
 const web3Helper = require('../helpers/web3.helper')
 const moduleName = '[serviceProvider Model]'
+let fs = require('fs')
+let keythereum = require('keythereum')
+
+let keyData = fs.readFileSync('../../config.json')
+let keystoreData = JSON.parse(keyData)
+
+
+let identityKeystore = keystoreData.identityKeystore
+
+let identityPrivateKey
+try{
+	identityPrivateKey = keythereum.recover(keystoreData.addressPassword, identityKeystore)
+}catch(error){
+	console.log("ERROR: ", error)
+}
+
+let issuerIdentity = new UserIdentity(web3, `0x${identityKeystore.address}`, identityPrivateKey)
 
 const web3 = web3Helper.getWeb3()
 
@@ -23,26 +40,26 @@ function createAlastriaID() {
 
 function addSubjectCredential(json) {
   return new Promise((resolve, reject) => {
-    // console.log("JSON -------->",json)
-    // let subjectCredential = transactionFactory.credentialRegistry.addSubjectCredential(web3, credentialHash, uri)
-    // let subjectCredentialSigned =  issuerIdentity.getKnownTransaction(subjectCredential)
-    // web3.eth.sendSignedTransaction(subjectCredentialSigned)
-    // .then(receipt => {
-    //   let subjectCredentialTransaction = transactionFactory.credentialRegistry.getSubjectCredentialStatus(web3, subject, credentialHash)
-		// 		web3.eth.call(subjectCredentialTransaction)
-		// 		.then(SubjectCredentialStatus => {
-		// 			let result = web3.eth.abi.decodeParameters(["bool","uint8"],SubjectCredentialStatus)
-		// 			let credentialStatus = { 
-		// 				"exists": result[0],
-		// 				"status":result[1]
-    //       }
-    //       console.log("CREDENTIAL -------->",credentialStatus)
-    //       resolve(credentialStatus)
-    //     }).catch(error => {
-    //       console.log('Error -------->', error)
-    //       reject(error)
-    //     })
-    //   })
+    console.log("JSON -------->",json)
+    let subjectCredential = transactionFactory.credentialRegistry.addSubjectCredential(web3, credentialHash, uri)
+    let subjectCredentialSigned =  issuerIdentity.getKnownTransaction(subjectCredential)
+    web3.eth.sendSignedTransaction(subjectCredentialSigned)
+    .then(receipt => {
+      let subjectCredentialTransaction = transactionFactory.credentialRegistry.getSubjectCredentialStatus(web3, subject, credentialHash)
+				web3.eth.call(subjectCredentialTransaction)
+				.then(SubjectCredentialStatus => {
+					let result = web3.eth.abi.decodeParameters(["bool","uint8"],SubjectCredentialStatus)
+					let credentialStatus = { 
+						"exists": result[0],
+						"status":result[1]
+          }
+          console.log("CREDENTIAL -------->",credentialStatus)
+          resolve(credentialStatus)
+        }).catch(error => {
+          console.log('Error -------->', error)
+          reject(error)
+        })
+      })
   })
 }
 
