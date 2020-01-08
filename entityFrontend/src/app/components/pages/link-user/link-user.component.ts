7b18bebf5c7709cc03af786bd022ff6f3a82143d
Samuel <sanchezlopezsamuel@gmail.com>
2020-01-08 16:11:23 +0100

    add user form commponent in link user

--- entityFrontend/src/app/components/pages/link-user/link-user.component.ts
+++ entityFrontend/src/app/components/pages/link-user/link-user.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+// SERVICES
+import { UserService } from 'src/app/services/user/user.service';
+
+// MODELS
+import { User } from './../../../models/user/user.model';
+
+
 @Component({
   selector: 'app-link-user',
   templateUrl: './link-user.component.html',
   styleUrls: ['./link-user.component.css']
 })
 export class LinkUserComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.user = {
+      name: '',
+      surname: '',
+      password: ''
+    };
   }
 
 }
