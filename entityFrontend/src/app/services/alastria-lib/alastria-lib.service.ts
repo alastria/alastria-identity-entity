b3112981261f20a483e5a4980d7aec53a12faa0d
Samuel <sanchezlopezsamuel@gmail.com>
2020-01-22 11:04:53 +0100

    install alastria-lib and create alastria-lib-services

---
+++ entityFrontend/src/app/services/alastria-lib/alastria-lib.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { tokensFactory } from 'alastria-identity-lib';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlastriaLibService {
+
+  constructor() { }
+
+  createPresentationRequest(header: any, payload: any) {
+    return tokensFactory.tokens.createPresentationRequest(
+      header.kid, payload.iss, payload.pr['@context'], payload.pr.procUrl, payload.pr.procHash, payload.pr.data, payload.nbf, payload.jti
+    );
+  }
+}
