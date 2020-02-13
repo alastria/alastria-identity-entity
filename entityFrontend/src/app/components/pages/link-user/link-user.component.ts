36fc83602d60876f4de8eff4edbc183e5d554998
Samuel <sanchezlopezsamuel@gmail.com>
2020-02-13 09:23:35 +0100

    fix signJWT

--- entityFrontend/src/app/components/pages/link-user/link-user.component.ts
+++ entityFrontend/src/app/components/pages/link-user/link-user.component.ts
@@ -207,8 +207,7 @@ export class LinkUserComponent implements OnInit {
       ];
 
       const presentationRequest = this.alastriaLibService.createPresentationRequest(alastriaLibJson.header, alastriaLibJson.payload);
-      // TODO GET PRIVATE KEY
-      const presentationRequestSigned = this.alastriaLibService.signJWT(presentationRequest, alastriaLibJson.privateKey);
+      const presentationRequestSigned = this.alastriaLibService.signJWT(presentationRequest.payload, alastriaLibJson.privateKey);
       this.qrData = presentationRequestSigned;
     } catch (error) {
       console.error(error);
